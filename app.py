e84195a1c97eea69778ae3f463061efc5d2a6baa
ramesh86babu <85245791+ramesh86babu@users.noreply.github.com>
2024-08-26 21:50:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ b2=st.sidebar.button("login")
 b3=st.sidebar.button("logout")
 
 with st.expander("Open me"):
-    st.image("nit1.jpg") 
+    st.image("https://github.com/ramesh86babu/streamlit-demo/blob/main/nit1.JPG") 
